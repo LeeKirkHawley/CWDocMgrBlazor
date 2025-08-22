a85ad5076581bb4340d399f1473d81c099ccf9a3
Kirk Hawley <leekirkhawley@gmail.com>
2025-08-22 08:06:49 -0700

    make the Details action show details instead of DocumentName

--- CWDocMgrBlazor/Mapper.cs
+++ CWDocMgrBlazor/Mapper.cs
@@ -9,19 +9,15 @@ namespace CWDocMgrBlazor
     {
         public static MapperConfiguration RegisterMaps(ILoggerFactory? loggerFactory)
         {
-            // Create configuration options first
             var configurationOptions = new MapperConfigurationExpression();
 
-            // Configure the mappings
             configurationOptions.CreateMap<Models.DocumentModel, SharedLib.ViewModels.DocumentVM>()
                 .ForMember(dest => dest.DocumentDate, opt => opt.MapFrom(src => src.DocumentDate.ToUniversalTime().ToString("yyyy-MM-dd")));
             configurationOptions.CreateMap<SharedLib.ViewModels.DocumentVM, Models.DocumentModel>()
                 .ForMember(dest => dest.DocumentDate, opt => opt.MapFrom(src => DateTime.Parse(src.DocumentDate).ToUniversalTime()));
-
             configurationOptions.CreateMap<DocumentModel, DocumentUploadVM>()
                 .ForMember(dest => dest.DateString, opt => opt.MapFrom(src => src.DocumentDate.ToString("yyyy-MM-dd")));
 
-            // Create the configuration with logger (if provided)
             MapperConfiguration mappingConfig;
             mappingConfig = new MapperConfiguration(configurationOptions, loggerFactory);
             return mappingConfig;
